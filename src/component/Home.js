143caa8f288c11f3bbc6037ca8134d07dab181bc
박종범 <jbpark03@gmail.com>
2025-04-14 14:33:24 +0900

    has two social login links

---
+++ src/component/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <h2>소셜 로그인 링크</h2>
+      <button onClick={googleLogin}>구글</button>
+      <button onClick={githubLogin}>깃허브</button>
+    </div>
+  )
+}
