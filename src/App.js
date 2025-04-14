955b89ac8f598707df26e2e1a9cf4af093851459
박종범 <jbpark03@gmail.com>
2025-04-14 16:28:34 +0900

    routes to Dashboard compo'

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import { Route, BrowserRouter as Router, Routes }
   from 'react-router-dom';
 import './App.css';
 import { Home } from './component/Home';
+import { Dashboard } from './component/Dashboard';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </Router>
   );
