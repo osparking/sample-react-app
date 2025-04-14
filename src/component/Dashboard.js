e5b180495d0c438f4430efeca314282a69c29d5e
박종범 <jbpark03@gmail.com>
2025-04-14 21:12:49 +0900

    div contains user's name and email para'

--- src/component/Dashboard.js
+++ src/component/Dashboard.js
@@ -4,6 +4,10 @@ export const Dashboard = () => {
   return (
     <div>
       <h1>Dashboard</h1>
+      <div>
+        <p><strong>이름: </strong></p>
+        <p><strong>이메일: </strong></p>
+      </div>
     </div>
   )
 }
