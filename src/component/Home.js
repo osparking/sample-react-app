d314451501835d5410c7a7b7ec6f882b28592a94
박종범 <jbpark03@gmail.com>
2025-04-15 21:48:25 +0900

    put all three tags at the center

--- src/component/Home.js
+++ src/component/Home.js
@@ -9,8 +9,17 @@ export const Home = () => {
     localStorage.setItem("social", "github");
     window.location.href = "http://localhost:8080/oauth2/authorization/github";
   }
+
+  const centerize = {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: '100vh' // 100 % of view port height
+  }
+
   return (
-    <div>
+    <div style={centerize}>
       <h2>소셜 로그인 링크</h2>
       <button onClick={googleLogin}>구글</button>
       <button onClick={githubLogin}>깃허브</button>
